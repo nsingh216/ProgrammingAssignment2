05d22f978353d99d169f561b0a8dc62943c1fed8
nsingh216 <niveditasingh216@gmail.com>
2018-01-07 08:11:25 -0600

    Adding in my solution
    updating with my solution code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## this returns the supplied matrix
+  get <- function() x
+  ## getReverse will be null until a setReverse operation has been completed for this matrix
+  setReverse <- function(solve) m <<- solve
+  ## this returns the reverse matrix, or null if it hasnt been calculated yet
+  getReverse <- function() m
+  ## return all the functions
+  list(set = set, get = get,
+       setReverse = setReverse,
+       getReverse = getReverse)
+ 
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getReverse() 
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  my_matrix <- x$get()
+  m <- solve(my_matrix, ...)
+  x$setReverse(m)
+  m
 }
